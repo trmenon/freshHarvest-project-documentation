fedca26692c8f32ad4f929263d38592ee9ed9343
trmenon <trmenon911@gmail.com>
2022-05-10 09:58:44 +0530

    Memory router trial-initials-3

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -5,7 +5,7 @@ import { ProfileWidget, LaunchWidget} from '../widgets';
 
 function FreshHarvest(){
     return (
-        <MemoryRouter initialEntries={["http://trmenon.github.io/freshHarvest-project-documentation"]}>
+        <MemoryRouter initialEntries={[""]}>
             <Routes>
                 <Route path="/" element={<Gate/> }/>
                 <Route path="/docs" element={<Docs/>}>
